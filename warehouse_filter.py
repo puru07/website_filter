c64eaa897ec38c762377ebadcd105ad3943dd043
puru <pururastogi@gmail.com>
2023-12-24 22:09:13 +0000

    add the feature to resume from where it was left

--- warehouse_filter.py
+++ warehouse_filter.py
@@ -7,12 +7,21 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-def find_words_in_websites(websites_df, words_to_check,output_csv_file):
+def find_words_in_websites(websites_df, words_to_check, input_csv_file):
     websites_with_words = []
     total_websites = len(websites_df.index)
     print('total websites: ',total_websites)
+
     for index, row in websites_df.iterrows():
         site = row['Website']
+        
+        # Skip websites that have already been checked
+        if row.get('checked') == True:
+            if index%10 == 0:
+                percentage_completion = ((index*100)//total_websites)
+                print(".............percent completed:  ", percentage_completion)
+            continue
+
         try:
             response = requests.get(site)
             soup = BeautifulSoup(response.text, 'html.parser')
@@ -21,43 +30,54 @@ def find_words_in_websites(websites_df, words_to_check,output_csv_file):
             if any(word.lower() in soup.get_text().lower() for word in words_to_check):
                 websites_with_words.append(site)
                 websites_df.at[index, new_column_name] = True
-                print(site)
             else:
                 websites_df.at[index, new_column_name] = False
-        except requests.exceptions.RequestException as e:
-            print(f"Error accessing {site}: {e}")
-            websites_df.at[index, new_column_name] = False
 
+            # Mark the website as checked
+            websites_df.at[index, 'checked'] = True
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error accessing {site}")
+            websites_df.at[index, new_column_name] = False
+        
         if index%10 == 0:
             percentage_completion = ((index*100)//total_websites)
             print(".............percent completed:  ", percentage_completion)
-            print("updating csv file of name ",output_csv_file)
-            websites_df.to_csv(output_csv_file, index=False)
+            print("updating csv file of name ",input_csv_file)
+            websites_df.to_csv(input_csv_file, index=False)
+            print('update done')
 
     return websites_with_words
 
+
 # Read the CSV file
-input_csv_file = 'Warehouse_US_List_PA_list.csv'
-output_csv_file = 'output_Warehouse_US_List_PA_list.csv'
+input_csv_file = 'output_Warehouse_US_List_PA_list.csv'
 new_column_name = 'contain_keyword'
+
+# Specify the list of words to check
 words_to_check = ['warehouse', 'warehousing','pick']
 print('the keywords are ')
 print(words_to_check)
 
+
+# read the CSV file
 print('reading file: ', input_csv_file)
 websites_df = pd.read_csv(input_csv_file)
 
-# Specify the list of words to check
 
+# Add columns for new_column_name and 'checked' in the DataFrame if not present
+if new_column_name not in websites_df.columns:
+    websites_df[new_column_name] = None
+if 'checked' not in websites_df.columns:
+    websites_df['checked'] = False
 
-# Add a column for new_column_name in the DataFrame
-websites_df[new_column_name] = None
 
 # Find websites with specified words and update the DataFrame
-result = find_words_in_websites(websites_df, words_to_check,output_csv_file)
+result = find_words_in_websites(websites_df, words_to_check, input_csv_file)
 
-# Save the updated DataFrame to a new CSV file
-websites_df.to_csv(output_csv_file, index=False)
+
+# Save the updated DataFrame to the same CSV file
+websites_df.to_csv(input_csv_file, index=False)
 
 print("Websites mentioning specified words:")
 for site in result:
